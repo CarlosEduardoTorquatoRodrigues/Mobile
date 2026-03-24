f606e3e35f7bfc8b6bf2f986c644af741df4936d
CarlosEduardoTorquatoRodrigues <145603092+CarlosEduardoTorquatoRodrigues@users.noreply.github.com>
2026-03-24 20:38:49 +0000

    ok

--- projetoMobile/src/screens/HomeScreen.js
+++ projetoMobile/src/screens/HomeScreen.js
@@ -5,7 +5,7 @@ import homeStyles from '../styles/homeStyles';
 export default function HomeScreen() {
   return (
     <View style={homeStyles.container}>
-      <Text style={homeStyles.title}>Bem-vindo ao aplicativo</Text>
+      <Text style={homeStyles.title}>Bem-vindo</Text>
       <Text style={homeStyles.message}>
         Utilize o menu de navegação para acessar as telas de modais e as listas com rolagem.
       </Text>
